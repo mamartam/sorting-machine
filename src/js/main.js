9ed5adb0ea21c926ca4838bb161d9640223f4750
mamartam <marta.masiukk@gmail.com>
2026-04-07 12:45:19 +0300

    add function for sorting data - bubble sorting function

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,7 @@ generateArrayBtn.addEventListener("click", (event) => {
   mainArray.splice(0);
   generateArray(5, mainArray);
   generateElement(mainArray);
+  console.log(bubbleSort(mainArray));
 });
 
 function randomaizer(min, max) {
@@ -31,4 +32,17 @@ function generateElement(array) {
   for (let i = 0; i < array.length; i++) {
     createColumn(array[i]);
   }
+}
+let bubble;
+function bubbleSort(array) {
+  for (let i = array.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        bubble = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = bubble;
+      }
+    }
+  }
+  return array;
 }
