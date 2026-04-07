2be9eafd209d620ab2ab37ef36735a5b7044bff0
mamartam <marta.masiukk@gmail.com>
2026-04-07 12:22:00 +0300

    create functions for generating rundom numbers, also for creating array, and generating elements based on data from array

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 const mainContainer = document.querySelector(".main");
-const array = [50, 80, 20, 100, 40];
+const generateArrayBtn = document.querySelector(".generate-array-btn");
+
+const mainArray = [];
 
 function createColumn(height) {
   let newColumn = document.createElement("div");
@@ -8,6 +10,25 @@ function createColumn(height) {
   mainContainer.appendChild(newColumn);
 }
 
-for (let i = 0; i < array.length; i++) {
-  createColumn(array[i]);
+generateArrayBtn.addEventListener("click", (event) => {
+  mainContainer.innerHTML = "";
+  mainArray.splice(0);
+  generateArray(5, mainArray);
+  generateElement(mainArray);
+});
+
+function randomaizer(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateArray(lenght, array) {
+  for (let i = 0; i < lenght; i++) {
+    array.push(randomaizer(1, 200));
+  }
+}
+
+function generateElement(array) {
+  for (let i = 0; i < array.length; i++) {
+    createColumn(array[i]);
+  }
 }
